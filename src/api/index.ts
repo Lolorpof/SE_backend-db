8949612e11fb7e854ecd8d7abf4e980ca6efa49c
Lolorpof <lolorpof@gmail.com>
2025-01-21 00:56:35 +0700

    minor edit

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,7 +3,6 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import { drizzlePool } from "../db/conn";
-import { testTable } from "../db/schema";
 
 const port = process.env.BACKEND_PORT; //6977
 
@@ -18,11 +17,6 @@ app.use([
 
 app.get("/", async (req, res) => {
   res.json({ success: true, msg: "hello world" });
-});
-
-app.post("/", async (req, res) => {
-  await drizzlePool.insert(testTable).values({ name: "LMAO" });
-  res.json({ success: true, msg: "inserted!" });
 });
 
 app.listen(port, () => {
