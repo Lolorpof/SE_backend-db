23ef2c425556c10fe47731e5239095f65fadbcd7
Lolorpof <lolorpof@gmail.com>
2025-01-21 03:01:51 +0700

    create template for api

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,6 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
-import { drizzlePool } from "../db/conn";
 
 const port = process.env.BACKEND_PORT; //6977
 
---
+++ src/api/services/usersService.ts
@@ -0,0 +1,6 @@
+export class usersService {
+  // logic here
+  static async getAllUsers(): Promise<any> {
+    return "hello world";
+  }
+}
