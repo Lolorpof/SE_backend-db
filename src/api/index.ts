82b192dde908c4ba60b194aceb1fbf857282ce81
pubest <110964000+pubestpubest@users.noreply.github.com>
2025-01-26 18:08:44 +0700

    body-parser init

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,11 +2,13 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import bodyParser from "body-parser";
 import { drizzlePool } from "../db/conn";
 
 const port = process.env.BACKEND_PORT; //6977
 
 const app = express();
+app.use(bodyParser.json());
 app.use([
   cors({
     origin: `http://localhost:${process.env.FRONTEND_PORT}`,
