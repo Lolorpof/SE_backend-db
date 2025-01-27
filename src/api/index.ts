3b416923b2c6510827187cfb33040538b9c55b95
pubestpubest <pubestpubest@gmail.com>
2025-01-27 13:18:41 +0000

    Add Swagger documentation setup for API

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,10 +5,38 @@ import helmet from "helmet";
 import bodyParser from "body-parser";
 import { drizzlePool } from "../db/conn";
 import postRoutes from "../routes/postRoutes";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 const port = process.env.BACKEND_PORT; //6977
 
 const app = express();
+
 app.use(bodyParser.json());
+
+
+//swagger definition
+const swaggerOption = {
+  definition:{
+    openapi: "3.0.0",
+    info:{
+      title: "API Documentation",
+      version: "1.0.0",
+      description: "A simple Express Post API"  
+    }
+  },
+  servers: [
+    {
+      url: "http://localhost:6977",
+      description: "Development server"
+    }
+  ],
+  apis: ["index.ts"],
+}
+
+const swaggerSpec = swaggerJSDoc(swaggerOption);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.use([
   cors({
     origin: `http://localhost:${process.env.FRONTEND_PORT}`,
